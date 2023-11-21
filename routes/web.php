9ac3080d38754b4dade831d464ed3f7f2848a63e
Khoi Nguyen <nguyenkhoi2227@gmail.com>
2023-11-21 18:12:19 +0700

    First Commit

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::group(['prefix' => 'admin'],function(){
+    Route::post('/login', [AdminController::class,'login']);
+});
+
+Route::group(['prefix' => 'user'],function(){
+    Route::post('/login', [UserController::class,'login']);
+});
